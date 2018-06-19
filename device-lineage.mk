50f803ccc0e64878ed1d10677aa8c97aa351b5c1
Max Bires <jbires@google.com>
2018-06-19 10:05:00 -0700

    wahoo: Adding VENDOR_SECURITY_PATCH level definition
    Change-Id: I0d923d4eb67ddef3be81409676472fe520bbacf5

--- device-lineage.mk
+++ device-lineage.mk
@@ -39,6 +39,9 @@ PRODUCT_VENDOR_KERNEL_HEADERS := device/google/wahoo/kernel-headers
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
 
+# Setting vendor SPL
+VENDOR_SECURITY_PATCH := "2020-09-05"
+
 # Shared java libs
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
