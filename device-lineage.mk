0c0c1770c5c1b3fb5b2e882ce35bd1d749847724
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-13 12:00:28 -0600

    wahoo: enable snap and configure overlay
    Change-Id: Ieda6914c95d877842933d6df463a9f475f24ee21

--- device-lineage.mk
+++ device-lineage.mk
@@ -14,6 +14,13 @@ PRODUCT_PACKAGES += \
 # Kernel headers
 PRODUCT_VENDOR_KERNEL_HEADERS := device/google/wahoo/kernel-headers
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
+
 # Shared java libs
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
+
+# Snap
+PRODUCT_PACKAGES += \
+    Snap
