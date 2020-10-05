LineageOS/android_device_google_wahoo

wahoo: Enable EUICC Sim
Change-Id: If43e859c80e69b35b08c1097ad268cf4411d2d8b

device-lineage.mk
@@ -1,2 +1,6 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-lineage
+
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.euicc.xml

deedwar/device_google_wahoo

device-lineage.mk
@@ -1,3 +1,7 @@
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Kernel headers
 PRODUCT_VENDOR_KERNEL_HEADERS := device/google/wahoo/kernel-headers
 
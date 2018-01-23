baae715b39171a192c7af959ac84c369439359c2
Christopher N. Hesse <raymanfx@gmail.com>
2018-01-23 23:42:55 +0100

    wahoo: Enable EUICC Sim
    Change-Id: If43e859c80e69b35b08c1097ad268cf4411d2d8b

--- device-lineage.mk
+++ device-lineage.mk
@@ -1,3 +1,7 @@
+# EUICC feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Kernel headers
 PRODUCT_VENDOR_KERNEL_HEADERS := device/google/wahoo/kernel-headers
 
